6df007555937d40d8658164a5a5c5376516072df
Vihanga Muthumala <149398329+vihanga02@users.noreply.github.com>
2024-06-08 01:18:20 +0530

    chore: Add Nano-Processor project details to Projects component

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,14 @@
 const Projects = () => {
     return (
         <div className="projects">
+            <h1>Projects</h1>
             <div className="nano-processor">
+                <img src="https://github.com/vihanga02/my_portfolio/blob/main/src/img/basys-3-0.png?raw=true" alt="board" />
+                <h2>Nano-Processor</h2>
+                <p>The assigned lab task is to design and develop a 4-bit nanoprocessor capable of executing 4 instructions. This includes several components that need to be developed or extended from the previous labs and this is a group project for Computer Organization and Digital Design module for semester 2. We used Vivado to all the coding and simulatioin works.</p>
+                <p className="project-git"><a href="https://github.com/vihanga02/Nano-Processor.git"></a>Check this out!</p>
+            </div>
+            <div className="grocery-shop">
                 <img src="" alt="" />
             </div>
         </div>
