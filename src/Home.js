81785eca9f13d9667392a619a39fd74656de4e86
Vihanga Muthumala <149398329+vihanga02@users.noreply.github.com>
2024-06-07 21:23:13 +0530

    chore: Update Home component with new image URL and developer name

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,16 @@
 const Home = () => {
     return (
-        <div className="content">
-            <img src="https://github.com/vihanga02/my_portfolio/blob/main/src/img/desktop-wallpaper-nillia-webdesign-web-development-management.jpg?raw=true" alt="developer-img" />
-            <p id="name">Vihanga Muthumala</p>
+        <div className="home">
+            <div className="home-content">
+                <img src="https://github.com/vihanga02/my_portfolio/blob/main/src/img/desktop-wallpaper-nillia-webdesign-web-development-management.jpg?raw=true" alt="developer-img" />
+                <p id="name">Vihanga Muthumala</p>
+                <p id="developer-tag">&lt;Developer&gt;</p>
+            </div>
+            <div className="thumbs">
+                <div className="about-me-img">
+                    <img src="" alt="" />
+                </div>
+            </div>
         </div>
     )
 }
--- src/NavBar.js
+++ src/NavBar.js
@@ -4,7 +4,7 @@ import React from 'react';
 const NavBar = () => {
     return (
         <nav>
-            <h1>Vihanga Muthuamala</h1>
+            <img src="https://github.com/vihanga02/my_portfolio/blob/main/src/img/360_F_353903472_pXZ24g7X9HHyZOAdOsHGARXhOuhns1DX.jpg?raw=true" alt="VM" />
             <div className="links">
                 <Link to="/">Home</Link>
                 <Link to="/">About Me</Link>
