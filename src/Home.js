7c346bc562ac857135e0b09f22b4c5442413dc29
Vihanga Muthumala <149398329+vihanga02@users.noreply.github.com>
2024-06-07 21:05:06 +0530

    feat: Update Home component with new image URL and developer name

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,8 @@
 const Home = () => {
     return (
         <div className="content">
-            <img src="./img/desktop-wallpaper-nillia-webdesign-web-development-management.jpg" alt="developer-img" />
+            <img src="https://github.com/vihanga02/my_portfolio/blob/main/src/img/desktop-wallpaper-nillia-webdesign-web-development-management.jpg?raw=true" alt="developer-img" />
+            <p id="name">Vihanga Muthumala</p>
         </div>
     )
 }
