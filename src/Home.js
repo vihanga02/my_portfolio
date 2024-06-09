cf9fa18e56a21ecf7fde36e155d0de4acc55e96c
Vihanga Muthumala <149398329+vihanga02@users.noreply.github.com>
2024-06-10 01:15:50 +0530

    Update package dependencies and add framer-motion library

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,24 @@
 import { Link } from "react-router-dom";
 import "./Home.css";
+import { motion } from "framer-motion"
 
-const Home = () => {
+const variants = {
+    hidden: { opacity: 0, y: -20 },
+    visible: { opacity: 1, y: 0 },
+  };
+
+  const Home = () => {
     return (
         <div className="home">
             <div className="home-content">
                 <img src="https://github.com/vihanga02/my_portfolio/blob/main/src/img/Picsart_24-06-09_11-34-03-523.png?raw=true" alt="me" />
-                <p id="developer-tag">&lt;Developer&gt;</p>
-                <p id="name">Vihanga .<span> Muthu</span> + mala</p>
-                
+                <p id="name"><span>I am </span>Vihanga  Muthumala</p>
+                <div className="developer-tag">
+                    <p id="tag0">&lt;Developer&gt;</p>
+                    <p id="tag1">&lt;Developer&gt;</p>
+                    <p id="tag2">&lt;Developer&gt;</p>
+                    <p id="tag3">&lt;Developer&gt;</p>
+                </div>
             </div>
             <div className="thumbs">
                 <div className="about-me-img">
