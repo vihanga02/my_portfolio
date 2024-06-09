522daf92a9d4626bbf253a2fd31efc6d7d78f098
Vihanga Muthumala <149398329+vihanga02@users.noreply.github.com>
2024-06-09 10:51:50 +0530

    chore: Update NavBar links in NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,15 +1,16 @@
 import {Link} from 'react-router-dom';
 import React from 'react';
+import './NavBar.css';
 
 const NavBar = () => {
     return (
         <nav>
             <img src="https://github.com/vihanga02/my_portfolio/blob/main/src/img/360_F_353903472_pXZ24g7X9HHyZOAdOsHGARXhOuhns1DX.jpg?raw=true" alt="VM" />
             <div className="links">
-                <Link to="/">Home</Link>
-                <Link to="/">About Me</Link>
-                <Link to="/">Projects</Link>
-                <Link to="/">Contact</Link>
+                <Link to="/my_portfolio/">Home</Link>
+                <Link to="/my_portfolio/about">About</Link>
+                <Link to="/my_portfolio/projects">Projects</Link>
+                <Link to="/my_portfolio/contact">Contact</Link>
             </div>
         </nav>
     );
