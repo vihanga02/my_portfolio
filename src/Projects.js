1ac443a455d573b949401e21559db128743546c0
Vihanga Muthumala <149398329+vihanga02@users.noreply.github.com>
2024-06-13 01:46:07 +0530

    Update Projects.css to adjust the width of dummy-project images and center the text in project cards

--- src/Projects.js
+++ src/Projects.js
@@ -38,13 +38,14 @@ const Projects = () => {
                         </div>         
                     </div> 
                     <div className='view-more-less'>
-                        <button onClick={handleViewMore} className='view-less-button'>View Less</button>
+                        <button onClick={handleViewMore} className='view-more-less-button less-button'>View Less <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" id="arrow-drop-down-circle"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M12 4c4.41 0 8 3.59 8 8s-3.59 8-8 8-8-3.59-8-8 3.59-8 8-8m0-2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 13-4-4h8z"></path></svg></button>
                     </div>     
                 </div>
             )}
             {!showAll && (
                 <div className="view-more-less">
-                    <button onClick={handleViewMore} className='view-more-button'>View More</button>
+                    <button onClick={handleViewMore} className='view-more-less-button'>View More <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" id="arrow-drop-down-circle"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M12 4c4.41 0 8 3.59 8 8s-3.59 8-8 8-8-3.59-8-8 3.59-8 8-8m0-2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 13-4-4h8z"></path></svg></button>
+                    
                 </div>
             )}
             
