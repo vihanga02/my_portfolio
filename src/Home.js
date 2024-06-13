cab9181026a55704808708143665e84f1f387fc6
Vihanga Muthumala <149398329+vihanga02@users.noreply.github.com>
2024-06-13 23:13:33 +0530

    Update Home component layout and lazy load images

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,20 @@
 import "./Home.css";
+import { motion } from "framer-motion";
 
-
-  const Home = () => {
+const Home = () => {
     return (
         <div className="home">
             <div className="home-content">
                 <img src="https://github.com/vihanga02/my_portfolio/blob/main/src/img/Picsart_24-06-09_11-34-03-523.png?raw=true" alt="me" />
-                <p id="name"><span>I am</span>Vihanga  Muthumala</p>
                 <div className="developer-tag">
                     <p id="tag0">&lt;Developer&gt;</p>
                     <p id="tag1">&lt;Developer&gt;</p>
                     <p id="tag2">&lt;Developer&gt;</p>
                     <p id="tag3">&lt;Developer&gt;</p>
                 </div>
+                <p id="name">
+                    <span> I am</span>Vihanga  Muthumala
+                </p>
             </div>
         </div>
     )
